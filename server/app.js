a16d05e7e1aac6be33227073bb06e2178089f155
Kindra Kinney <kindrakinney@gmail.com>
2020-05-26 18:50:55 -0700

    fixed require models error

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = 5000
 
-require('./models/user');
+
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost/instastage", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+require('./models/user');
 
 app.listen(PORT,()=>{
     console.log("Server is running on", PORT)
