f0e2f389bcce3cbb71842069f4236c0700d8c914
Ariel Huynh <arielhuynh@ymail.com>
2020-05-26 21:37:42 -0700

    fixed placements

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = 5000
 
+app.use(express.json())
 
-app.use(require("./routes/auth"))
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost/instastage", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/instastage", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+
 require('./models/user');
+app.use(require("./routes/auth"))
 
 app.listen(PORT,()=>{
     console.log("Server is running on", PORT)
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,20 @@
 const express= require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = mongoose.model("User");
-
 
 router.get("/", (req,res) => {
     res.send("hello")
 })
 
 router.post('/signup', (req,res)=>{
-    const {name, email, password} = req.body
-    if (!email || !password || !name){
-        return res.status(422).json({error:"please add all the fields"})
+    const {name,email,password} = req.body
+    if(!email || !password || !name) {
+        return res.status(422).json({error: "please add info for all the fields"})
     }
     User.findOne({email:email})
-    .then((savedUser)=>{
-        if(savedUser){
+    .then((savedUser) => {
+        if(savedUser) {
             return res.status(422).json({error: "user already exists with that email"})
         }
         const user = new User({
@@ -23,12 +22,16 @@ router.post('/signup', (req,res)=>{
             password,
             name
         })
+
         user.save()
-        .then(user=>{
+        .then(user => {
+            res.json({message: "saved successfully!"})
+        })
+        .catch(err => {
             console.log(err)
         })
     })
-    .catch(err=>{
+    .catch(err => {
         console.log(err)
     })
 })
