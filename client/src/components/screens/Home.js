425af967e5b3fc70b74cfc712be911d8c986af50
Kindra Kinney <kindrakinney@gmail.com>
2020-05-27 12:41:04 -0700

    added cloudinary api to createPost

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                     <img src="https://unsplash.com/photos/JgOeRuGD_Y4" alt="canyon"/>
                 </div>
                 <div className="card-content">
-                    <i class="material-icons">favorite_border</i>
+                    <i className="material-icons">favorite_border</i>
                     <h6>title</h6>
                     <p>this is amazing</p>
                     <input type="text" placeholder="add comment" />
