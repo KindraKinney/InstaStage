8fd3b9ae6f75c155aa175e2e473c8ba684f4b640
Kindra Kinney <kindrakinney@gmail.com>
2020-05-27 08:50:37 -0700

    homepage and profile UI finished

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,22 @@ import React from "react"
 
 const Home = () => {
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Vera</h5>
+                <div className="card-image">
+                    <img src="https://unsplash.com/photos/JgOeRuGD_Y4" alt="canyon"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite_border</i>
+                    <h6>title</h6>
+                    <p>this is amazing</p>
+                    <input type="text" placeholder="add comment" />
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,7 +1,41 @@
 import React from "react"
 
 const Profile = () => {
- 
+    return(
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
+            <div style={{
+
+                    display:"flex",
+                    justifyContent: "space-around",
+                    margin:"18px 0px",
+                    borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img style={{width: "160", height:"160px", borderRadius: "80px"}} 
+                    src="https://unsplash.com/photos/pg_WCHWSdT8" alt="girl" />
+                </div>
+                <div>
+                    <h4>Vera Anderson</h4>
+                    <div style={{display:"flex", justifyContent:"space-between", width:"108%"}}>
+                        <h6>40 posts</h6>
+                        <h6>40 followers</h6>
+                        <h6>40 following</h6>
+
+                    </div>
+                </div>
+
+
+            </div>
+            
+            <div className="gallery">
+                <img className="item" src="https://unsplash.com/photos/pg_WCHWSdT8" alt="girl"  />
+                <img className="item" src="https://unsplash.com/photos/pg_WCHWSdT8" alt="girl"  />
+                <img className="item" src="https://unsplash.com/photos/pg_WCHWSdT8" alt="girl"  />
+                <img className="item" src="https://unsplash.com/photos/pg_WCHWSdT8" alt="girl"  />
+
+            </div>
+        </div>
+    )
        
     
 }
