547dedc273c3e49b60f73cb6ff0ab55821569596
Kindra Kinney <kindrakinney@gmail.com>
2020-05-27 17:28:07 -0700

    client build and mlab connection

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/instastage", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://user:password1@ds127944.mlab.com:27944/heroku_mkjllzxl", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 require('./models/user');
 require("./models/post");
 app.use(require("./routes/auth"))
